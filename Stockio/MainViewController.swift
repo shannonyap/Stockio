5fcf01c6bf25a762de44f2b2b1dd2e63ab1e7875
Shannon Yap <shannonyap@utexas.edu>
2016-07-22 23:01:37 -0500

    Added searchButton for mainVC.

--- Stockio/MainViewController.swift
+++ Stockio/MainViewController.swift
@@ -9,15 +9,21 @@
 import UIKit
 import DrawerController
 
-class MainViewController: UIViewController, UITableViewDelegate {
+class MainViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.tableView.delegate = self
+        tableView.delegate = self
+        tableView.dataSource = self
         
         initVCWithNavBarPresets("Watchlist")
+        addSearchBarButtonItem()
+        
+        self.tableView.tableFooterView = UIView()
+        displayEmptyWatchListLabel()
+        
         // Do any additional setup after loading the view.
     }
 
@@ -25,7 +31,38 @@ class MainViewController: UIViewController, UITableViewDelegate {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func displayEmptyWatchListLabel() {
+        let emptyWatchlist = UILabel(frame: CGRect(x: 0, y: 0, width: self.view.bounds.size.width, height: self.view.bounds.size.height * 0.3))
+        emptyWatchlist.center = self.view.center
+        emptyWatchlist.text = "Your Watchlist is empty"
+        emptyWatchlist.textAlignment = .Center
+        emptyWatchlist.font = UIFont(name: "Suranna", size: 20.0)
+        self.view.addSubview(emptyWatchlist)
+    }
 
+    func addSearchBarButtonItem() {
+        let barButton = UIBarButtonItem(image: UIImage(named: "searchIcon"), style: .Plain, target: self, action: #selector(didSelectSearchBarButtonItem(_:)))
+        barButton.tintColor = UIColor.blackColor()
+        self.navigationItem.setRightBarButtonItem(barButton, animated: true)
+    }
+    
+    func didSelectSearchBarButtonItem(sender: UIBarButtonItem) {
+        print("searchButton pressed")
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 0
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .Default, reuseIdentifier: "reuseIdentifier")
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
     /*
     // MARK: - Navigation
 
